4ca81571e775b38a25fe821f69b90829788a1b3d
Trish-Aguarin <trish_ann_aguarin@dlsu.edu.ph>
2024-10-20 19:02:10 +0800

    Update FileHandling.h
    added count in the parameter of indexCheck

--- c/FileHandling.h
+++ c/FileHandling.h
@@ -12,7 +12,7 @@ typedef struct {
 } Coordinates;
 
 // Function to check the index of a node in the reference array
-int indexCheck(char reference[MAX_ROWS][MAX_COLS], char temp[MAX_NAME_LEN]);
+int indexCheck(char reference[MAX_ROWS][MAX_COLS], char temp[MAX_NAME_LEN], int count);
 
 // Function to calculate Euclidean distance between two nodes
 double calculateEuclideanDistance(Coordinates node1, Coordinates node2);
