7a14dfe9458e570a4becaa9623d2bcf1c53e16c1
Trish-Aguarin <trish_ann_aguarin@dlsu.edu.ph>
2024-10-20 16:55:09 +0800

    lettters path yipee

--- c/GBFS.c
+++ c/GBFS.c
@@ -1,64 +1,3 @@
-/*
-function GreedyBestFirstSearch(Graph, start_node, goal_node)
-{
-    Create a priority queue (min-heap) to store nodes based on their heuristic values
-    PriorityQueue pq;
-
-    Initialize all nodes to have infinite heuristic values
-    foreach node in Graph
-        node.heuristic_value = INFINITY;
-
-     Set the heuristic value of the start node (typically to zero or calculated heuristic)
-    start_node.heuristic_value = heuristic(start_node, goal_node);
-
-    Push the start node to the priority queue
-    pq.push(start_node);
-
-    Create an array to keep track of visited nodes
-    bool visited[] = {false};
-
-     Main loop of the algorithm
-    while (!pq.empty())
-    {
-         Get the node with the lowest heuristic value
-        current_node = pq.pop();
-
-         Mark the current node as visited
-        visited[current_node] = true;
-
-         If the goal node is found, return the path or indicate success
-        if (current_node == goal_node)
-        {
-            return "Goal found!";
-        }
-
-         Iterate through all neighbors of the current node
-        foreach neighbor in current_node.neighbors
-        {
-             If the neighbor has not been visited
-            if (!visited[neighbor])
-            {
-                 Calculate the heuristic value for the neighbor
-                neighbor.heuristic_value = heuristic(neighbor, goal_node);
-
-                 Add the neighbor to the priority queue
-                pq.push(neighbor);
-            }
-        }
-    }
-
-     If the loop ends without finding the goal node, return failure
-    return "Goal not found";
-}
-
- Example of a heuristic function (e.g., Euclidean distance)
-function heuristic(node, goal_node)
-{
-    return sqrt(pow((node.x - goal_node.x), 2) + pow((node.y - goal_node.y), 2));
-}
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -69,7 +8,7 @@ function heuristic(node, goal_node)
 
 
 
-void greedyBestFirstSearch(int startIndex, int goalIndex, int adjMatrix[MAX_ROWS][MAX_COLS], Coordinates coords[MAX_ROWS], int numVertices) {
+void greedyBestFirstSearch(int startIndex, int goalIndex, int adjMatrix[MAX_ROWS][MAX_COLS], Coordinates coords[MAX_ROWS], int numVertices, char reference[MAX_ROWS][MAX_COLS]) {
     int openList[MAX_ROWS], closedList[MAX_ROWS] = {0}; // openList for neighbors, closedList for visited
     int parent[MAX_ROWS]; // to keep track of the path
     int openListCount = 0;
@@ -121,7 +60,7 @@ void greedyBestFirstSearch(int startIndex, int goalIndex, int adjMatrix[MAX_ROWS
             }
             printf("Final Path: ");
             for (i = pathLength - 1; i >= 0; i--) {
-                printf("%d ", finalPath[i]); // should print them in the correct order now
+                printf("%s ", reference[finalPath[i]]); // should print them in the correct order now in letters (wah so cool)
             }
             printf("\n");
 
@@ -150,7 +89,7 @@ void greedyBestFirstSearch(int startIndex, int goalIndex, int adjMatrix[MAX_ROWS
             if(adjMatrix[currentNode][i] != 0 && !closedList[i]){
                 // add da neighbor sa openList if wala pa don
                 isInOpenList = 0;
-                for(int j = 0; j < openListCount; j++){
+                for(j = 0; j < openListCount; j++){
                     if (openList[j] == i){
                         isInOpenList = 1;
                         break;
@@ -158,14 +97,11 @@ void greedyBestFirstSearch(int startIndex, int goalIndex, int adjMatrix[MAX_ROWS
                 }
                 if(!isInOpenList){
                     openList[openListCount++] = i;
-                    parent[i] = currentNode; // u set the parent of the current node, DIS WAS THE ERROR😭
+                    parent[i] = currentNode; // u set the parent of the current node
                 }
             }
         }
     }
     
-    
-    
-    // return -1; // if no goal node is found
     printf("goal aint found");
 }
