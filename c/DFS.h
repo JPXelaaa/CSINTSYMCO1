91186474aaa600f78a12160364d1351c84b104ea
aiellareigne <aiellareigne20@gmail.com>
2024-10-22 13:15:05 +0800

    Create DFS.h

---
+++ c/DFS.h
@@ -0,0 +1,8 @@
+#define MAX_ROWS 50
+#define MAX_COLS 50
+#define MAX_NAME_LEN 50
+
+// void DFSRecursion(int v, int visited[], char reference[MAX_ROWS][MAX_COLS],
+// int graph[MAX_ROWS][MAX_COLS], int numVertices, FILE *fp);
+
+void DFS(char reference[MAX_ROWS][MAX_COLS], int graph[MAX_ROWS][MAX_COLS], int numVertices, int startIndex, int goalIndex);
