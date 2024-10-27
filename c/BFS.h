9b3809ab4b615a2563e0359fe92c8930bbc007d7
Trish-Aguarin <trish_ann_aguarin@dlsu.edu.ph>
2024-10-27 13:29:37 +0800

    STILL TO FIX

--- c/BFS.h
+++ c/BFS.h
@@ -2,6 +2,6 @@
 #define MAX_COLS 50
 #define MAX_NAME_LEN 50
 
-void to_lowercase(char *str);
+void backtrackPath(int parent[], int goalIndex, int adjMatrix[MAX_ROWS][MAX_COLS], char reference[MAX_ROWS][MAX_COLS]);
 void BFS(char reference[MAX_ROWS][MAX_COLS], int graph[MAX_ROWS][MAX_COLS], int numVertices, int startIndex, int goalIndex);
 
