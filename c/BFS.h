f9ec83aafe401ce6b11e3a26eea38ddc220149ca
Trish-Aguarin <trish_ann_aguarin@dlsu.edu.ph>
2024-10-27 13:51:39 +0800

    Update BFS.h

--- c/BFS.h
+++ c/BFS.h
@@ -1,3 +1,6 @@
+#ifndef BFS_H
+#define BFS_H
+
 #define MAX_ROWS 50
 #define MAX_COLS 50
 #define MAX_NAME_LEN 50
@@ -5,3 +8,4 @@
 void backtrackPath(int parent[], int goalIndex, int adjMatrix[MAX_ROWS][MAX_COLS], char reference[MAX_ROWS][MAX_COLS]);
 void BFS(char reference[MAX_ROWS][MAX_COLS], int graph[MAX_ROWS][MAX_COLS], int numVertices, int startIndex, int goalIndex);
 
+#endif
