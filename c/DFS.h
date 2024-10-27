96e1d4fb98404d4a613319995f4229a510e7f5f3
Trish-Aguarin <trish_ann_aguarin@dlsu.edu.ph>
2024-10-27 13:32:29 +0800

    STILL TO FIX

--- c/DFS.h
+++ c/DFS.h
@@ -2,7 +2,4 @@
 #define MAX_COLS 50
 #define MAX_NAME_LEN 50
 
-// void DFSRecursion(int v, int visited[], char reference[MAX_ROWS][MAX_COLS],
-// int graph[MAX_ROWS][MAX_COLS], int numVertices, FILE *fp);
-
 void DFS(char reference[MAX_ROWS][MAX_COLS], int graph[MAX_ROWS][MAX_COLS], int numVertices, int startIndex, int goalIndex);
