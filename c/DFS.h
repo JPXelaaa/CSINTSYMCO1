380cecc70651781d8e15846b8b0d0e9ad21dc0ff
Trish-Aguarin <trish_ann_aguarin@dlsu.edu.ph>
2024-10-27 13:52:20 +0800

    Update DFS.h

--- c/DFS.h
+++ c/DFS.h
@@ -1,5 +1,10 @@
+#ifndef DFS_H
+#define DFS_H
+
 #define MAX_ROWS 50
 #define MAX_COLS 50
 #define MAX_NAME_LEN 50
 
 void DFS(char reference[MAX_ROWS][MAX_COLS], int graph[MAX_ROWS][MAX_COLS], int numVertices, int startIndex, int goalIndex);
+
+#endif
